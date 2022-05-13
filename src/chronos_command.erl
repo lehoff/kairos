5759ab4bb48fad26ea04a0de76b65643fdee0970
Josep Lluis Giralt D'Lacoste <josepgiraltdlacoste@gmail.com>
2022-05-13 08:26:28 +0100

    fix(adding-ci): pacify dialyzer

--- src/chronos_command.erl
+++ src/chronos_command.erl
@@ -7,7 +7,7 @@
 ]).
 
 -spec start_timer(non_neg_integer, chronos:timer_name()) ->
-    'ok' | {'error', term()}.
+    reference().
 start_timer(Duration, TimerName) ->
     erlang:start_timer(Duration, self(), TimerName).
 
@@ -15,7 +15,7 @@ start_timer(Duration, TimerName) ->
 cancel_timer(TRef) ->
     erlang:cancel_timer(TRef).
 
--spec execute_callback(chronos:callback()) -> 'ok'.
+-spec execute_callback(chronos:callback()) -> pid().
 execute_callback({Mod, Fun, Args}) ->
     % We spawn a function to execute the apply because we want to protect the timer
     % server against errors. Might need to log this.
